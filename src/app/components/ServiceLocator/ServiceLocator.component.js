abfc3f2ca803c4f46b17a80f89045f95855354e0
Jill Yan <yan95599@gmail.com>
2018-06-05 18:08:15 +0800

    use list component in customized module

--- src/app/components/ServiceLocator/ServiceLocator.component.js
+++ src/app/components/ServiceLocator/ServiceLocator.component.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { List } from '@talend/react-containers';
 
 function ServiceLocator(props) {
 
-    return (<div>This is a customized Service Locator module.</div>);
+    const servicelocatorProps = {
+        "collectionId": "servicelocators",
+        "list": {
+            "columns": [
+                { "key": "id", "label": "ID" },
+                { "key": "status", "label": "Status" },
+                { "key": "uptime", "label":"Uptime"},
+                { "key": "endpoint",  "label": "Service Endpoint" },
+                { "key": "namespace", "label": "Namespace" }
+            ]
+        },
+        "toolbar": {
+            "filter": {
+                "placeholder": "Find a servicelocator"
+            },
+            "sort": {
+                "options": [
+                    { "id": "label", "name": "Name" },
+                    { "id": "status", "name": "status" }
+                ]
+            },
+            "pagination": {}
+        },
+        "actions" :{
+            "left":["servicelocator:refresh"]
+        },
+        "initialState": {
+            "sortOn": "status"
+        }
+    };
+
+    return (<List id="ServiceLocator" {...servicelocatorProps}/>);
 }
 
 ServiceLocator.displayName="ServiceLocator";
