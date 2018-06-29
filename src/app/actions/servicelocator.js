c8ab704133dea053103759425743574c450b2437
Jill Yan <yan95599@gmail.com>
2018-06-29 12:02:08 +0800

    add action button and pagination

--- src/app/actions/servicelocator.js
+++ src/app/actions/servicelocator.js
@@ -1,5 +1,10 @@
 import {actions} from '@talend/react-cmf';
-import {GETTING_SERVICELOCATORS, ERROR_GETTING_SERVICELOCATORS} from '../constants/servicelocators';
+import {
+    GETTING_SERVICELOCATORS,
+    ERROR_GETTING_SERVICELOCATORS,
+    REFRSHING_SERVICELOCATORS,
+    ERROR_REFRSHING_SERVICELOCATORS
+} from '../constants/servicelocators';
 
 export function fetchServicelocators() {
     return actions.http.get('/servicelocators.json', {
@@ -11,6 +16,20 @@ export function fetchServicelocators() {
         // collectionId is the key where the result will be stored in app state
         cmf: {
             collectionId: 'servicelocators',
+        },
+    });
+}
+
+export function refreshServicelocators() {
+    return actions.http.get('/servicelocators.json', {
+        // action type to dispatch before fetch
+        onSend: REFRSHING_SERVICELOCATORS,
+        // action type to dispatch on fetch error
+        onError: ERROR_REFRSHING_SERVICELOCATORS,
+        // CMF action config
+        // collectionId is the key where the result will be stored in app state
+        cmf: {
+            collectionId: 'servicelocators'
         },
     });
 }
